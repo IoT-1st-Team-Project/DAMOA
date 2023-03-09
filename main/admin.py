41b0550703969d17a7a942720d15cf7fa2a7fc0c
Seongmo <43720012+gu0112@users.noreply.github.com>
2023-03-09 13:46:37 +0900

    3/8 구성모 club, board, reply 모델 추가

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Club
+from .models import Club, Board, Reply
 
 # class BoardAdmin(admin.ModelAdmin):
 #     search_fields = ['category']
 
 admin.site.register(Club)
+admin.site.register(Board)
+admin.site.register(Reply)
 
 
 
