afbedc560c7dd8d2ad710d686f5582a661917eba
Khancording <digitwk@gmail.com>
2023-03-10 12:02:52 +0900

    KHS 3/10 12:00 복구

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from main import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include('main.urls')),  # / 페이지에 해당하는 urlpatterns 
+    path('common/', include('common.urls')),
+    path('main/', views.main, name='main'), # main/ 페이지에 해당하는 urlpatterns
+    path('login/', views.main, name='login'),
+]
+
---
+++ main/views.py
@@ -0,0 +1,43 @@
+from .models import Club, Board
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+
+# Create your views here.
+def index(request):
+    """
+    첫 화면 출력
+    """
+    return render(request, 'main/index.html')
+
+def main(request):
+    """
+    메인 화면 출력
+    """
+    club_list=Club.objects.order_by('-id')
+    context={'club_list':club_list}
+    return render(request, 'main/main.html', context)
+
+
+def board_list(request):
+    '''
+    board 출력
+    '''
+    board_list=Board.objects.order_by('-id')
+    context={'board_list':board_list}
+    return render(request, 'main/board_list.html',context)
+
+def detail(request, board_id):
+    '''
+    board내용 출력
+    '''
+    board=get_object_or_404(Board, pk=board_id)
+    context={'board':board}
+    return render(request, 'main/board_datail.html',context)
+
+
+def login(request):
+    """
+    로그인 화면 출력
+    """
+    
+    return render(request, 'templates/common/login.html')   
