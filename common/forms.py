b14c0b44275b250c7f801c1c21d130165ed88465
Khancording <digitwk@gmail.com>
2023-03-13 17:40:52 +0900

    KH 3/13 17:40

--- common/forms.py
+++ common/forms.py
@@ -7,6 +7,11 @@ from .models import CustomUser
 # UserCreateionForm은 username(이름), pw1, pw2 속성을 가짐
 
 class CustomUserForm(UserCreationForm):
+    email = forms.EmailField(label="이메일")
+    username = forms.CharField(max_length = 15, label="아이디")
+    name = forms.CharField(max_length = 15, label="성명")
+    age = forms.IntegerField(label="나이")
+    ad = forms.CharField(max_length = 50, label="거주지")
 
     class Meta(UserCreationForm.Meta) :
         model = CustomUser
