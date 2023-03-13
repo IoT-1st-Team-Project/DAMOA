e3963996cb66a15521e8872be9795cca8f40609c
Khancording <digitwk@gmail.com>
2023-03-13 13:00:53 +0900

    KH 3/13 13:00

--- config/settings.py
+++ config/settings.py
@@ -31,15 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "common",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "common.apps.CommonConfig",
     "main.apps.MainConfig",
 ]
+
+AUTH_USER_MODEL = "common.CustomUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
