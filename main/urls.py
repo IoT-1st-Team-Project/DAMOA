347ee9b417ca13a2aea845fe22bcb88eb6b74e3a
Seongmo <43720012+gu0112@users.noreply.github.com>
2023-03-14 14:20:26 +0900

    3/14 리스트 화면

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('main/', views.main, name='main'),
     path('main/board_list/', views.board_list, name='board_list'),
-    path('main/board_list/<int:board_id>/', views.detail, name='datail'),
-
+    path('main/board_list/<int:board_id>/', views.detail, name='detail'),
+ 
 ]
